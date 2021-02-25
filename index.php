fb19e496b84761458375137d638d84b0468ed470
Fábián Gábor <info@fabiangabor.com>
2021-02-25 16:18:15 +0100

    src no protocol

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ require_once('config/db.php');
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation.min.css" integrity="sha256-ogmFxjqiTMnZhxCqVmcqTvjfe1Y/ec4WaRj/aQPvn+I=" crossorigin="anonymous">
     -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/combine/npm/foundation-sites@6.6.3/dist/css/foundation.min.css,npm/foundation-sites@6.6.3/dist/css/foundation-float.min.css,npm/foundation-sites@6.6.3/dist/css/foundation-prototype.min.css">
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/combine/npm/foundation-sites@6.6.3/dist/css/foundation.min.css,npm/foundation-sites@6.6.3/dist/css/foundation-float.min.css,npm/foundation-sites@6.6.3/dist/css/foundation-prototype.min.css">
     <meta name="theme-color" content="#fafafa">
     <link rel="manifest" href="site.webmanifest">
     <link rel="icon" type="image/png" href="favicon-32x32.png">
@@ -32,8 +32,8 @@ require_once('config/db.php');
 <body>
 <?php include('register.php'); ?>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/js/foundation.min.js" integrity="sha256-pRF3zifJRA9jXGv++b06qwtSqX1byFQOLjqa2PTEb2o=" crossorigin="anonymous"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+<script src="//cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/js/foundation.min.js" integrity="sha256-pRF3zifJRA9jXGv++b06qwtSqX1byFQOLjqa2PTEb2o=" crossorigin="anonymous"></script>
 <script>
     // $(document).foundation();
 </script>
