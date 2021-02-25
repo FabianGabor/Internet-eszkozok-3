016a634877966f17f3aff10a802d3c584a3828ea
Fábián Gábor <info@fabiangabor.com>
2021-02-25 10:38:08 +0100

    init

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Fábián Gábor
+ * CXNU8T
+ * https://github.com/FabianGabor/Internet-eszkozok-2
+ */
+ ?>
+<!doctype html>
+<html class="no-js" lang="hu">
+
+<head>
+    <meta charset="utf-8">
+    <title>Portal</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation.min.css" integrity="sha256-ogmFxjqiTMnZhxCqVmcqTvjfe1Y/ec4WaRj/aQPvn+I=" crossorigin="anonymous">
+    -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/combine/npm/foundation-sites@6.6.3/dist/css/foundation.min.css,npm/foundation-sites@6.6.3/dist/css/foundation-float.min.css,npm/foundation-sites@6.6.3/dist/css/foundation-prototype.min.css">
+    <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+<?php include('register.php'); ?>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/js/foundation.min.js" integrity="sha256-pRF3zifJRA9jXGv++b06qwtSqX1byFQOLjqa2PTEb2o=" crossorigin="anonymous"></script>
+<script>
+    // $(document).foundation();
+</script>
+
+</body>
+
+</html>
